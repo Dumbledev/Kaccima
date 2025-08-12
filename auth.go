40f8c51b4f707b9d4dad774e6d383539ba4dd27b
dumbledev <atm9465@gmail.com>
2025-08-12 17:19:42 +0100

    .

--- auth.go
+++ auth.go
@@ -211,7 +211,7 @@ func signInHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	session.Options = &sessions.Options{
-		Domain:   "http://83.229.83.17:8000",
+		Domain:   "localhost",
 		Path:     "/",
 		MaxAge:   86400 * 7,
 		HttpOnly: true,
